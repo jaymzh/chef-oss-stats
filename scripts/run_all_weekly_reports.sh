ef355316b3f4a457b20e3ad9921dac5338789ee3
Phil Dibowitz <phil@ipom.com>
2026-04-22 16:52:30 -0700

    Weekly report
    Signed-off-by: Phil Dibowitz <phil@ipom.com>

--- scripts/run_all_weekly_reports.sh
+++ scripts/run_all_weekly_reports.sh
@@ -10,7 +10,7 @@ die() {
 }
 
 echo -n "Have you already updated meeting for last week? [Y/n] "
-read ans
+read -r ans
 if ! [[ "$ans" =~ [Yy] ]]; then
     die "Please do that first" 
 fi
@@ -20,13 +20,9 @@ date=$(date '+%Y-%m-%d')
 echo "Running Pipeline Visibility Report"
 ./bin/pipeline_visibility_stats \
     --skip adhoc --skip private --skip release \
+    --skip-repos cac-meeting-tracker \
     --buildkite-org chef-oss --github-org chef \
     -o "pipeline_visibility_reports/${date}.md"
-# the old one...
-# ../oss-stats/bin/pipeline_visibility_stats \
-#    --skip adhoc --skip private --skip release \
-#    --provider expeditor --github-org chef \
-#    -o "pipeline_visibility_reports/${date}.md"
 
 echo "Running Meeting Report"
 ./bin/meeting_stats -m generate
@@ -38,4 +34,5 @@ echo "Running CI Report"
 ./scripts/run_weekly_repo_reports.sh -o "repo_reports/${date}.md" "${@}"
 
 echo "Running external contributors Report"
-./scripts/non-progress-stats.rb "${@}" > external_contributors_reports/${date}.md
+./scripts/non-progress-stats.rb "${@}" \
+    > "external_contributors_reports/${date}.md"
